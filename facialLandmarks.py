8edea074c17a517e4fc90452ab63373ef2f9fe1b
Rishabh Gupta <rishabh.gupta2@oyorooms.com>
2018-10-04 16:03:48 +0530

    added buffer for reactange

--- facialLandmarks.py
+++ facialLandmarks.py
@@ -19,13 +19,10 @@ faces = faceDetector(grayImage, 1)
 for (i, face) in enumerate(faces):
 	facialLandmarks = facialLandmarkPredictor(grayImage, face)
 	facialLandmarks = face_utils.shape_to_np(facialLandmarks)
-	
 	(x, y, w, h) = face_utils.rect_to_bb(face)
-	cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-	
+	cv2.rectangle(image, (x+1, y+1), (x+w+1, y+h+1), (0, 255, 0), 2)
 	cv2.putText(image, '#{}'.format(i+1), (x, y-10),
 			cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-	
 	for (x, y) in facialLandmarks:
 		cv2.circle(image, (x, y), 1, (0, 0, 255), -1)
 
